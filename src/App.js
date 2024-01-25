e020b709ed4f864ba8afacd23b02a0bb2570a2e3
DaBuggins <109602512+DaBuggins@users.noreply.github.com>
2024-01-25 09:03:53 +0000

    Create App.js
    Initial commit with game logic

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Pokecard from "./Pokecard";
+import Pokedex from "./Pokedex";
+import Pokegame from "./Pokegame";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Pokegame />
+    </div>
+  );
+}
