f1b7735b493250dfa026698a8aa94f37f1ee7920
DaBuggins <109602512+DaBuggins@users.noreply.github.com>
2024-01-25 09:06:10 +0000

    Create Pokecard.jsx

---
+++ src/Pokecard.jsx
@@ -0,0 +1,15 @@
+import "./Pokecard.css";
+
+export default function Pokecard({ name, id, type, exp }) {
+  const POKE_API = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/`;
+
+  let imgSrc = `${POKE_API}${id}.png`;
+  return (
+    <div className="Pokecard">
+      <h1 className="Pokecard-title"> {name}</h1>
+      <img src={imgSrc} alt={name} />
+      <div className="Pokecard-data">Type: {type} </div>
+      <div className="Pokecard-data">Worth {exp} EXP</div>
+    </div>
+  );
+}
