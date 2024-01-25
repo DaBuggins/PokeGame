0b947fd56178ed6e372e0c6a96b7f6f89feae3c0
DaBuggins <109602512+DaBuggins@users.noreply.github.com>
2024-01-25 10:35:21 +0000

    Update Pokecard.jsx
    Added div to image for styling

--- src/Pokecard.jsx
+++ src/Pokecard.jsx
@@ -7,7 +7,9 @@ export default function Pokecard({ name, id, type, exp }) {
   return (
     <div className="Pokecard">
       <h1 className="Pokecard-title"> {name}</h1>
-      <img src={imgSrc} alt={name} />
+      <div className="Pokecard-image">
+        <img src={imgSrc} alt={name} />
+      </div>
       <div className="Pokecard-data">Type: {type} </div>
       <div className="Pokecard-data">Worth {exp} EXP</div>
     </div>
